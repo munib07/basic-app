8a2175c2dd1d5f2958105cd955fff46ee416e2e9
jigsrajwani <jigyasarajwani2@gmail.com>
2024-07-26 14:09:13 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/message')
+    axios.get('https://basicapp-backend.azurewebsites.net/api/message')
       .then(response => {
         setMessage(response.data.message);
       })
